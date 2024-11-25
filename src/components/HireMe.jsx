cda08348ab55a3368e704ebe745e14140f1814a0
Aman Kumar <amankumar322230@gmail.com>
2024-11-25 18:10:23 +0530

    Email Changes

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -3,6 +3,22 @@ import { motion } from "framer-motion";
 import { personalInfo } from "../constants";
 
 const HireMe = () => {
+  const handleClick = () => {
+    const email = personalInfo.email;
+    const userAgent = navigator.userAgent.toLowerCase();
+
+    // Check if the user is on a mobile device
+    const isMobile = /android|iphone|ipad|ipod/i.test(userAgent);
+
+    if (isMobile) {
+      // If on mobile, open with Gmail app
+      window.location.href = `mailto:${email}`;
+    } else {
+      // If on desktop/laptop, open Gmail in the browser
+      window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email}`, '_blank');
+    }
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0, y: -50 }}
@@ -13,13 +29,12 @@ const HireMe = () => {
     >
       <div className="w-24 h-24 relative">
         <div className="border-t-4 border-blue-500 border-solid rounded-full animate-spin absolute inset-0"></div>
-        <a
-          href={`https://mail.google.com/mail/?view=cm&fs=1&to=${personalInfo.email}`}
+        <button
+          onClick={handleClick}
           className="flex justify-center items-center absolute left-4 top-4 green-pink-gradient w-16 h-16 rounded-full"
-          target="_blank"
         >
           <span className="text-white text-[14px] p-1 font-normal">Hire Me</span>
-        </a>
+        </button>
       </div>
     </motion.div>
   );
